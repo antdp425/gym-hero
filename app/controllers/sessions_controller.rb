29309590485dd3fadd19d3451967052e10c2a484
antdp425 <antdp425@aol.com>
2020-06-15 02:16:23 -0400

    adds tradt'l login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,30 @@
 class SessionsController < ApplicationController
+   
+   def new
+   end
+
+   def create
+      binding.pry
+      if gym = Gym.find_by(email: session_params[:email])
+         if gym.authenticate(session_params[:password])
+            session[:user_id] = gym.id
+            redirect_to gym
+         else
+            render :new
+         end
+      else
+         render :new
+      end
+   end
+   
    def destroy
       reset_session
       redirect_to root_path
+   end
+
+   private
+
+   def session_params
+      params.require(:gym).permit(:email, :password)
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: "gyms#new", as: "signup"
   get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy", as: "logout"
 
 end
