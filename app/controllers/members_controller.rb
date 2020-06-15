32548ce098804845fde041f552b7222cb21bbc99
antdp425 <antdp425@aol.com>
2020-06-15 09:42:51 -0400

    gadds: member#index,new,create,show,edit,update actions/rviews (where applicable)

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,37 @@
 class MembersController < ApplicationController
+   def index
+      @members = current_user.members.all.count
+   end
+
+   def new
+      @member = current_user.members.build
+   end
+
+   def create
+      @member = current_user.members.build(member_params)
+      if @member.save
+         redirect_to @member
+      else
+         render :new
+      end
+   end
+
+   def show
+      @member = current_user.members.find_by(id: params[:id])
+   end
+
+   def edit
+      @member = current_user.members.find_by(id: params[:id])
+   end
+
+   def update
+      @member = current_user.members.find_by(id: params[:id])
+      @member.update(member_params)
+      redirect_to @member
+   end
+
+   private
+   def member_params
+      params.require(:member).permit(:name,:email,:age,:phone_number)
+   end
 end
