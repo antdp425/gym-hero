56a81e31de7048aa7a1cbbeb727143c303848064
antdp425 <antdp425@aol.com>
2020-06-15 01:13:39 -0400

    adds: current_user + logged_in helper methods, adds user to session on successful signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+   helper_method :current_user, :logged_in?
+
    def home
+   end
+
+   private
+   
+   def logged_in?
+      redirect_to root_path unless !!session[:user_id]
+   end
+
+   def current_user
+      @current_gym ||= Gym.find_by(id: session[:user_id])
    end
 end
--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,16 +1,18 @@
 class GymsController < ApplicationController
+   before_action :logged_in?, only: [:show]
+
    def new
       @gym = Gym.new
    end
 
    def create
       @gym = Gym.create(gym_params)
-      binding.pry
+      session[:user_id] = @gym.id 
       redirect_to @gym
    end
 
    def show
-      @gym = Gym.find_by(id: params[:id]) 
+      @gym = Gym.find_by(id: params[:id])
    end
 
    private
