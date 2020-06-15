08a0b47f9fdb3ab8aa409f2aec2b1c40b115d974
antdp425 <antdp425@aol.com>
2020-06-15 05:29:20 -0400

    adds: gymclass#index,new,create,show,edit,update actions/rviews (where applicable)

--- app/controllers/gym_classes_controller.rb
+++ app/controllers/gym_classes_controller.rb
@@ -1,2 +1,37 @@
 class GymClassesController < ApplicationController
+   def index
+      @classes = current_user.gym_classes.all.count
+   end
+
+   def new
+      @class = current_user.gym_classes.build
+   end
+
+   def create
+      @class = current_user.gym_classes.build(class_params)
+      if @class.save
+         redirect_to @class
+      else
+         render :new
+      end
+   end
+
+   def show
+      @class = current_user.gym_classes.find_by(id: params[:id])
+   end
+
+   def edit
+      @class = current_user.gym_classes.find_by(id: params[:id])
+   end
+
+   def update
+      @class = current_user.gym_classes.find_by(id: params[:id])
+      @class.update(class_params)
+      redirect_to @class
+   end
+
+   private
+   def class_params
+      params.require(:gym_class).permit(:name,:description,:duration,:max_size)
+   end
 end
