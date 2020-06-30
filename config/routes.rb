f4b483e8a009228d9bec3ef7e4f98dda141ab3da
antdp425 <antdp425@aol.com>
2020-06-30 14:08:30 -0400

    adds logic to show alert or to render partial for neew scheduled_class for scheduled_classes#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,55 @@
 Rails.application.routes.draw do
-  resources :gyms
-  resources :sessions
+  resources :gyms, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :members
   resources :gym_classes do 
     resources :scheduled_classes, only: [:index, :new, :create, :destroy]
   end
+
+  ################################################################
   
   #Github Authentication
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   
-  #Homepage
+  ################################################################
+
+  # Home
+  ## Not Signed In
   root "application#home"
-  
-  #Login/Logout
-  get '/login', to: "sessions#new", as: "login"
-  post '/login', to: "sessions#create"
-  delete '/logout', to: "sessions#destroy", as: "logout"
-  
-  #Gym SignUp + Dashboard + Today Page
+
+  ## Signed In
   get "/dashboard", to: "gyms#dashboard", as: "dashboard"
-  get '/signup', to: "gyms#new", as: "signup"
+
+  ## Signed In -- Today View
   get "/today", to: "gyms#today", as: "todays_classes"
   
-  # Deletes
+  ################################################################
+
+  # Login/Logout
+  get '/login', to: "sessions#new", as: "login"
+  delete '/logout', to: "sessions#destroy", as: "logout"
+
+  ################################################################
+  
+  # Gym SignUp
+  get '/signup', to: "gyms#new", as: "signup"
+
+  ################################################################
+
+  # Destroys
   ## GymClass
   delete "/gym_classes/:id", to: "gym_classes#destroy", as: "destroy_gym_class"
   
   ## Member
   delete "/members/:id", to: "members#destroy", as: "destroy_member"
 
-  # Cancels a class (destroys all scheduled_classes that match a certain time)
+  ## Cancels a class (destroys all scheduled_classes that match a certain time)
   delete "/gym_classes/:gym_class_id/scheduled_classes/:id", to: "scheduled_classes#destroy", as: "cancel_class"
 
+################################################################
 
-  #Catchall if invalid route/URL
+# Catchall if invalid route/URL
   get "*path", to: redirect("/")
+
+################################################################
 end
