b0cda6809951f777b7ca7df5aa7199de9f26a50a
antdp425 <antdp425@aol.com>
2020-09-08 15:10:12 -0400

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   ################################################################
   
   #Github Authentication
-  get '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   
   ################################################################
 
